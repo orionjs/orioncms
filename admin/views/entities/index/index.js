ebe23f5382719cd558621f8010b07eb2761187d9
Nicolás López <nicolaslopezj@me.com>
2015-01-18 12:29:49 -0300

    fixed #11

--- admin/views/entities/index/index.js
+++ admin/views/entities/index/index.js
@@ -4,8 +4,27 @@ Template.adminEntitiesIndex.helpers({
 			return this.entity.table;
 		}
 		return null;
+	},
+	showTable: function() {
+		return Session.get('adminEntitiesIndexShowTable');
 	}
 });
+
+Template.adminEntitiesIndex.rendered = function() {
+	Session.set('adminEntitiesIndexShowTable', true);
+
+	var toogleTable = function() {
+		Session.set('adminEntitiesIndexShowTable', false);
+		Meteor.setTimeout(function() {
+			Session.set('adminEntitiesIndexShowTable', true);
+		}, 10);
+	}
+
+	Deps.autorun(function () {
+		Router.current()
+		toogleTable();
+	});
+}
 
 Template.adminEntitiesIndexTableActions.helpers({
 	getEntity: function () {
