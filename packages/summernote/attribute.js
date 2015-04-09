d422bbde4faee12de2886787b847ec2dc8c5e943
Nicolás López <nicolaslopezj@me.com>
2015-04-09 14:30:16 -0300

    Summernote

---
+++ packages/summernote/attribute.js
@@ -0,0 +1,12 @@
+orion.attributes.registerAttribute('summernote', {
+  template: 'orionAttributesSummernote',
+  columnTemplate: 'orionAttributesSummernoteColumn',
+  getSchema: function(options) {
+    return {
+      type: String
+    };
+  },
+  valueOut: function() {
+    return this.find('.summernote').code();
+  }
+});
