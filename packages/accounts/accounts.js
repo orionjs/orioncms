50f13a4b208f14144be8c5df62fd820d137b6f27
Nicolás López <nicolaslopezj@me.com>
2015-04-15 14:30:20 -0300

    Default roles and forbidClientAccountCreation option

--- packages/accounts/accounts.js
+++ packages/accounts/accounts.js
@@ -1,18 +1,49 @@
 orion.accounts = {};
 
-orion.roles = Roles;
+/**
+ * Initialize the profile schema option with its default value
+ */
+Options.init('profileSchema', {
+  name: { type: String }
+});
 
-var defaultSchema = {
-  name: {
-    type: String
-  },
-}
-orion.options.init('profileSchema', defaultSchema);
-
+/**
+ * Updates the profile schema reactively
+ */
 Tracker.autorun(function () {
   orion.accounts.profileSchema = new SimpleSchema({
     profile: {
-      type: new SimpleSchema(orion.options.get('profileSchema'))
+      type: new SimpleSchema(Options.get('profileSchema'))
     }
   });
+});
+
+/**
+ * Initialize accounts options
+ * If there is no admin, we allow to create accounts
+ */
+Options.init('defaultRoles', []);
+Options.init('forbidClientAccountCreation');
+AccountsTemplates.configure({
+  forbidClientAccountCreation: !orion.adminExists
+});
+
+/**
+ * We will use listen instead of tracker because on client tracker starts after meteor.startup
+ */
+Options.listen('forbidClientAccountCreation', function(value) {
+  AccountsTemplates.configure({
+    forbidClientAccountCreation: orion.adminExists && value,
+  });
+})
+
+/**
+ * Adds the "name" field to the sign up form
+ */
+AccountsTemplates.addField({
+    _id: 'name',
+    type: 'text',
+    displayName: 'Name',
+    placeholder: 'Your name',
+    required: true,
 });
