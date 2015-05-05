0f7ee1c98db21988e18eba95c6a70f063f3d7365
Nicolás López <nicolaslopezj@me.com>
2015-05-05 10:12:11 -0300

    Created package

---
+++ packages/image-attribute/package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'orionjs:image-attribute',
+  summary: "Image attribute for orion",
+  version: "1.0.0",
+  git: "http://github.com/orionjs/orion"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.0');
+
+  api.use([
+    'orionjs:base@1.0.0',
+    'orionjs:attributes@1.0.0',
+    'orionjs:filesystem@1.0.0', 
+    'less'
+    ]);
+
+  api.addFiles([
+    'attribute.js',
+    ]);
+
+  api.addFiles([
+    ], 'client');
+});
