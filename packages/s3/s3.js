ab16e8ea2322d0beb1fd0d62874de35a2616175d
Ryan Harris <info@ryanbuiltthat.com>
2015-05-17 14:45:36 -0400

    S3 enhancement
    Added config for editable path

--- packages/s3/s3.js
+++ packages/s3/s3.js
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
   orion.filesystem.providerUpload = function(options, success, failure, progress) {
     S3.upload({
       files: options.fileList,
-      path: 'orionjs',
+      path: orion.config.get('AWS_S3_PATH', 'orionjs'),
     }, function(error, result) {
       if (error) {
         failure(new Meteor.Error('s3-error', 'Error uploading file'));
@@ -34,6 +34,7 @@ orion.config.add('AWS_API_KEY', 'aws')
 orion.config.add('AWS_API_SECRET', 'aws', { secret: true })
 orion.config.add('AWS_S3_BUCKET', 'aws')
 orion.config.add('AWS_S3_REGION', 'aws', { optional: true })
+orion.config.add('AWS_S3_PATH', 'aws', { optional: true })
 
 if (Meteor.isServer) {
   S3.config = {
