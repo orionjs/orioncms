f00b01f7084c50bf7564ce36436a72b776bb4b59
Eric Typaldos <etypaldos@pappasgroup.com>
2015-05-19 15:07:16 -0400

    UpdatedAt attribute

---
+++ packages/attributes/updated-at/updated-at.js
@@ -0,0 +1,28 @@
+orion.attributes.registerAttribute('updatedAt', {
+  previewTemplate: 'updatedAtPreview',
+  getSchema: function(options) {
+    return {
+      type: Date,
+      autoform: {
+        omit: true
+      },
+      autoValue: function() {
+        if (this.isUpdate || this.isInsert) {
+          return new Date;
+        } else if (this.isUpsert) {
+          return {$setOnInsert: new Date};
+        } else {
+          this.unset();
+        }
+      }
+    };
+  }
+});
+
+if (Meteor.isClient) {
+  ReactiveTemplates.helpers('attributePreview.updatedAt', {
+    date: function() {
+      return this.value && moment(this.value).format('LLL');
+    }
+  });
+}
