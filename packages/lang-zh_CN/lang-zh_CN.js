f9d82a50ecd9b745ba535c976ad5731d322dab64
xi3 <tianxi.liu@gmail.com>
2015-05-25 23:22:54 +0800

    Simplify Chinese language package for orionjs.

---
+++ packages/lang-zh_CN/lang-zh_CN.js
@@ -0,0 +1,175 @@
+mfPkg.syncAll({
+    zh_CN: {
+        accounts: {
+            text: '账户列表'
+        },
+        edit_roles: {
+            text: '编辑角色'
+        },
+        invalid_email: {
+            text: '邮箱地址无效'
+        },
+        passwords_must_match: {
+            text: '两次输入的密码必须一致'
+        },
+        no_permission_for_roles: {
+            text: '你无权修改用户角色'
+        },
+        invalid_invitation_code: {
+            text: '邀请码无效'
+        },
+        my_account: {
+            text: '我的账户'
+        },
+        name: {
+            text: '姓名'
+        },
+        your_name: {
+            text: '你的姓名'
+        },
+        email: {
+            text: '邮箱'
+        },
+        roles: {
+            text: '角色列表'
+        },
+        actions: {
+            text: '动作列表'
+        },
+        logout: {
+            text: '注销'
+        },
+        update_profile: {
+            text: '更新用户资料'
+        },
+        change_password: {
+            text: '修改密码'
+        },
+        invite: {
+            text: '邀请'
+        },
+        invitation_created_successfully: {
+            text: '邀请码生成成功'
+        },
+        back: {
+            text: '后退'
+        },
+        invite_another: {
+            text: '邀请他人'
+        },
+        select_new_user_roles: {
+            text: '为新用户选择角色'
+        },
+        optional_new_user_email: {
+            text: '新用户邮箱(可选)'
+        },
+        cancel: {
+            text: '取消'
+        },
+        login: {
+            text: '登录'
+        },
+        save: {
+            text: '保存'
+        },
+        register: {
+            text: '注册'
+        },
+        password_again: {
+            text: '再次输入密码'
+        },
+        select_user_roles: {
+            text: '为该用户分配角色'
+        },
+        create_a: {
+            text: '生成一个'
+        },
+        create: {
+            text: '生成'
+        },
+        no_permission: {
+            text: '权限不足'
+        },
+        delete: {
+            text: '删除'
+        },
+        are_you_sure_to_delete_this: {
+            text: '确定要删除？'
+        },
+        confirm: {
+            text: '确认'
+        },
+        update: {
+            text: '更新'
+        },
+        config: {
+            text: '配置'
+        },
+        dictionary: {
+            text: '字典'
+        },
+        create_new_page: {
+            text: '新增页面'
+        },
+        choose_template: {
+            text: '选择模板'
+        },
+        choose: {
+            text: '选择'
+        },
+        delete_page: {
+            text: '删除页面'
+        },
+        are_you_sure_to_delete_this_page: {
+            text: '你确定要删除该页面?'
+        },
+        pages: {
+            text: '页面列表'
+        },
+        update_page: {
+            text: '更新页面'
+        },
+        app_configuration: {
+            text: '应用配置'
+        },
+        remove: {
+            text: '移除'
+        },
+        choose_file: {
+            text: '选择文件'
+        },
+        file: {
+            text: '文件'
+        },
+        no_file: {
+            text: '缺少文件'
+        },
+        file_id_not_found: {
+            text: '找不到文件 {fileId} '
+        },
+        title: {
+            text: '标题'
+        },
+        item: {
+            text: '事项'
+        },
+        items: {
+            text: '事项列表'
+        },
+        users: {
+            text: '用户列表'
+        },
+        user: {
+            text: '用户'
+        },
+        error_uploading_file: {
+            text: '文件上传失败'
+        },
+        error_removing_file: {
+            text: '文件删除失败'
+        }
+    }
+}, {
+    'exportedAt': 1431194223035,
+    'updatedAt': 1431194216656
+});
