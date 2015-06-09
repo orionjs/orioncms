fc3d9bfa23551ba6a8e0ad391d147c84cc740885
Nicolás López <nicolaslopezj@me.com>
2015-06-09 19:48:56 -0300

    Fix account create button translation

--- packages/materialize/package.js
+++ packages/materialize/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'orionjs:materialize',
   summary: 'Materialize theme for orion admin',
-  version: '1.1.5',
+  version: '1.1.6',
   git: 'https://github.com/orionjs/orion'
 });
 
