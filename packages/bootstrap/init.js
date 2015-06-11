7c925545eebaba0bce4716e4fd45792992c8f56a
rwatts <Qjsld*1990>
2015-06-11 02:27:21 -0700

    Updates to the Sidebar
    - Updated the sidebar to use ReactiveTemplates better.
    - Created a ReactiveTemplate request for adminSidebar
    - Updated layout.html to use the reactive sidebar helper.
    - Created two additional templates for an option of dropdown or the standard sidebar.

--- packages/bootstrap/init.js
+++ packages/bootstrap/init.js
@@ -2,11 +2,13 @@ Options.init('homePath');
 Options.init('siteName');
 
 ReactiveTemplates.request('tabs', 'orionBootstrapTabs');
+ReactiveTemplates.request('adminSidebar');
+
 
 ReactiveTemplates.set('layout', 'orionBootstrapLayout');
 ReactiveTemplates.set('outAdminLayout', 'orionBootstrapOutAdminLayout');
 
-ReactiveTemplates.set('links', 'orionBootstrapSidebar');
+ReactiveTemplates.set('adminSidebar', 'orionBootstrapSidebar');
 ReactiveTemplates.set('login', 'orionBootstrapLogin');
 ReactiveTemplates.set('registerWithInvitation', 'orionBootstrapRegisterWithInvitation');
 
