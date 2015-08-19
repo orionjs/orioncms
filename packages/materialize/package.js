ddb2d5c6b2ab9f55a9f250baeb9c56e50f856bba
Nicolás López <nicolaslopezj@me.com>
2015-08-19 20:23:53 -0300

    Remove yield 'title' in out admin materialize
    Fix #280
    Fix
    http://forums.orionjs.org/t/problem-after-install-admin-not-available-de
    fer-error-on-client-side/111/2

--- packages/materialize/package.js
+++ packages/materialize/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'orionjs:materialize',
   summary: 'Materialize theme for orion admin',
-  version: '1.4.3',
+  version: '1.4.4',
   git: 'https://github.com/orionjs/orion'
 });
 
