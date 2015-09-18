3a9680f7a4a05119a851e56aa2ed2bc15ef3e0a6
Joris Röling <joris@bonboa.com>
2015-09-18 23:14:56 +0200

    never remove dictionaries

--- packages/dictionary/dictionary_server.js
+++ packages/dictionary/dictionary_server.js
@@ -1,8 +1,8 @@
 /**
  * If its on server, inserts the dictionary object
  */
-if (orion.dictionary.find(process.env.ORION_APPID?{_id:process.env.ORION_APPID}:{}).count() != 1) {
-  orion.dictionary.remove(process.env.ORION_APPID?{_id:process.env.ORION_APPID}:{});
+if (orion.dictionary.find(process.env.ORION_APPID?{_id:process.env.ORION_APPID}:{}).count() === 0) {
+  // orion.dictionary.remove(process.env.ORION_APPID?{_id:process.env.ORION_APPID}:{});
   orion.dictionary.insert(process.env.ORION_APPID?{_id:process.env.ORION_APPID}:{}, function(){
     console.log("Orion dictionary initialized");
   });
