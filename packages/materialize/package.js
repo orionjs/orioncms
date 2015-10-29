3255a7af4aacd0350ae8d2612651c50a7a1d649a
Nicolás López <nicolaslopezj@me.com>
2015-10-29 11:20:37 -0300

    Publish @sean-stanley patch

--- packages/materialize/package.js
+++ packages/materialize/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'orionjs:materialize',
   summary: 'Materialize theme for orion admin',
-  version: '1.6.0',
+  version: '1.6.1',
   git: 'https://github.com/orionjs/orion'
 });
 
