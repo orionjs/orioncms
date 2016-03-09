36afc6bdcc4685a529d329fdfd0a1001dfde3eac
Nicolás López <nicolaslopezj@me.com>
2016-03-09 03:21:22 -0300

    remove old versions

--- packages/lang-es/package.js
+++ packages/lang-es/package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.use('ecmascript');
+  api.use('ecmascript@0.3.0');
   api.use('orionjs:lang-en@1.8.0');
   api.imply('orionjs:lang-en');
 
